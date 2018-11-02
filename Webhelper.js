d32a8f56d57c8b6e522f060f38ceaa2f6e756229
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-02 08:03:48 -0400

    Update Webhelper.js

--- Webhelper.js
+++ Webhelper.js
@@ -1,5 +1,11 @@
 var buttonElement = document.getElementById("button1");
 
+window.onload = start;
+
+function start() {
+    setup();
+    town();
+}
 
 function story(text) {
     var currentStoryElement = document.getElementById("currentStory");
