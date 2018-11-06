0f17aeb86bf9d995bb5750088d8b19b95b91c9ff
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-06 12:43:01 -0500

    Create rpg-tollbooth.js

---
+++ rpg-tollbooth.js
@@ -0,0 +1,52 @@
+// rpg-tollbooth.js
+
+'use strict'  
+
+// core variables
+var messages = []; // for a sequence of messages
+var choices = [];
+var answer = null;
+// story function variables
+var name = null;
+var spin = 0;
+
+function checkAnswers(answer) {  // Matches Scenes  - replace with yours
+	if (answer == "Enter now") {
+		enterKingdomOfWisdom();
+	} 
+	else if (answer == "Sleep on it") {
+		wildNightmares();
+	}
+	else if (answer == "Tell mom") {
+		momCantSeeIt();
+	}
+}
+
+function enterKingdomOfWisdom() {
+	story("Undefined");
+	choices = ["Undefined"];
+	answer = setOptions(choices);
+}
+
+function wildNightmares() {
+	story("Undefined");
+	choices = ["Undefined"];
+	answer = setOptions(choices);
+}
+
+function momCantSeeIt() {
+	story("Undefined");
+	choices = ["Undefined"];
+	answer = setOptions(choices);
+}
+
+function tollbooth(){  // FIRST SCENE - WELCOMES PLAYER
+	var name = prompt("what is your name?");
+	story("You see a tollbooth appear from nowhere. \
+	\n On the window is a piece of paper\
+	\n That says \"Enter,  " + name + ".\"\
+	\n What do you do?");
+
+	choices = ["Enter now", "Sleep on it", "Tell mom"];
+	answer = setOptions(choices);
+}
