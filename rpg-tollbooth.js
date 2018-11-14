d989f04b926be74a9e5745f033db5c5db12c2287
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-14 11:39:23 -0500

    Added new scenes

--- rpg-tollbooth.js
+++ rpg-tollbooth.js
@@ -21,19 +21,19 @@ function checkAnswers(answer) {  // Matches Scenes  - replace with yours
 }
 
 function enterKingdomOfWisdom() { 
-  story("You are wandering along a road on a beautiful spring day, with green meadows and gentle rolling hills.  You don’t have a destination and you have plenty of time.\   \n Suddenly a tollbooth appears in front of you. There is a man in the booth waiting to speak to you.\ \nWhat do you do?");
-	choices = ["Enter Now", "Sleep on it", "Tell Mom"];
+  story("You walk up to the tollbooth.  The man says hello, and asks you for a penny. You reach into your pocket and find one. You give it to him. He says, “Welcome to the Kingdom of Wisdom!” as you walk by the booth.  Suddenly the landscape changes, and you see high, pointy mountains in the distance, and a forest directly ahead along the road as you walk.  You feel excited and scared at the same time. \ \nThe road splits into three, and there is a signpost with three signs, each with a name and pointing to a road.  Pointing left says Words, pointing center says Numbers, and pointing right says Whatever. Which road do you follow?");
+	choices = ["Left","Center","Right"];
 	answer = setOptions(choices);
 }
 function wildNightmares() {
-	story("Undefined");
-	choices = ["Undefined"];
+	story("You have horrible nightmares about what happens to people when they don’t make choices.  You find yourself in a corner, covered with dust and cobwebs, as your brain rots away.  You wake up in a cold sweat.  And run outside and find yourself again at the tollbooth on another sunny day.");
+	choices = ["Enter Now","Tell Mom"," "];;
 	answer = setOptions(choices);
 }
 
 function momCantSeeIt() {
-	story("Undefined");
-	choices = ["Undefined"];
+	story("You tell mom and she comes outside with you.  You point at the tollbooth.  She says she can’t see it.  You are very angry and frustrated and yell at her.  She sends you to bed and tells you to come out again when you are reasonable. ");
+	choices = ["Enter Now","Sleep on it"," "];
 	answer = setOptions(choices);
 }
 
