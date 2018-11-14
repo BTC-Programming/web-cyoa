dcd2710b34696bb454ca1d4924e2524429a64ea5
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-14 11:54:10 -0500

    Simplified Template

--- rpg.js
+++ rpg.js
@@ -33,47 +33,9 @@ function checkAnswers(answer) {  // Function names match story scenes. Replace w
  */ 
   
 /* function forest() {  // HOW TO SET A SCENE
- * story("There is a house made of candy\
- * \nThere is a fruit vender\
- * \nThere is a small opening in the trees with a ring of mushroom in the middle");
- * 
+ * story("There is a house made of candy\ \nThere is a fruit vender\ \nThere is a small opening in the trees with a ring of mushroom in the middle");
  * choices = ["Candy house", "Fruit vender", "Field", "Hang out here", "spin in a circle"];
  * answer = setOptions(choices);
  *    
  * }
  */ 
- 
-/* function spinInCircle() { // HOW CHANGE VALUE IF REPEATED CHOICE
- *    spin = spin + 1;
- *    if (spin == 3) {
- *         story("Why are you doing that?\
- *         \n It makes me a little nauseous just to watching you\n\n");
- *         forest();
- *     } else if (spin == 4) {
- *         story("Seriously, please stop\
- *         \nI can't keep watching this\n\n");
- *        forest();
- *    } else if (spin == 5) {
- *         story("Oh god I have to go\n\n");
- *         forest();
- *    } else if (spin > 5) {
- *         story("We are sorry but the narrator for this bit went home sick.\n\n");
- *         forest();
- *     } else {
- *        story("You let you arm fly freely at your side as you spin in a circle.\
- *         \nAround and around you go as the world blurs in front of you\
- *         \nyou get dizzy and fall to the ground\
- *         \nyou take a moment to gather your thoughts then you stand back up\n\n");
- *         forest();
- *     }
- * }
- */
- 
-/* function stayQuite() { // SEQUENCE OF MESSAGES WITH TIME DELAYS BETWEEN
- * messages=["so your too good to talk to me huh",
- *         "big old person too proud to talk to a pixie",
- *         " you hear the pixie fly away in disgust"];
- *    delayText(messages, 3000, field);
- * 
- * } 
- */
